87e5b1a22bb3b3d7405aa9d97b16a12abd7c54a0
GabrielOliveiraBorges <gabrieloborges01@gmail.com>
2025-03-03 20:40:12 -0300

    Add files via upload

---
+++ Exercícios de vetores 02/exerc 2.cs
@@ -0,0 +1,24 @@
+﻿int[] vet;
+int i, num, pares;
+
+Console.WriteLine("Digite a quantidade de numeros");
+num = int.Parse(Console.ReadLine());
+vet = new int[num];
+
+Console.WriteLine($"Digite {num} numeros");
+for (i = 0; i < num; i++)
+{
+    vet[i] = int.Parse(Console.ReadLine());
+}
+pares = 0;
+
+Console.WriteLine("Os numeros pares são: ");
+for (i = 0; i < num; i++)
+{
+    if (vet[i] % 2 == 0)
+    {
+        Console.WriteLine(vet[i]);
+        pares++;
+    }
+}
+Console.WriteLine($"a quantidade de numeros pares é {pares}");
