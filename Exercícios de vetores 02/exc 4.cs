ce389924da70126f4956535b2aac4a0080282957
GabrielOliveiraBorges <gabrieloborges01@gmail.com>
2025-03-03 20:42:45 -0300

    Add files via upload

---
+++ Exercícios de vetores 02/exc 4.cs
@@ -0,0 +1,24 @@
+﻿int[] vetor;
+int i, numeros, soma = 0;
+double media;
+
+Console.WriteLine("Digite o tamanho do vetor");
+numeros = int.Parse(Console.ReadLine());
+vetor = new int[numeros];
+
+Console.WriteLine($"Digite {numeros} numeros");
+for (i = 0; i < numeros; i++)
+{
+    vetor[i] = int.Parse(Console.ReadLine());
+}
+
+for (i = 0; i < numeros; i++)
+{
+    soma += vetor[i];
+}
+
+numeros = vetor.Length;
+
+media = soma / numeros;
+
+Console.WriteLine($"A média desses numeros é: {media}");
