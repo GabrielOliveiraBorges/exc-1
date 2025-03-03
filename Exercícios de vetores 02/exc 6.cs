9a8da92291ea0d6d0d453dcf1f28a434c11ce03f
GabrielOliveiraBorges <gabrieloborges01@gmail.com>
2025-03-03 20:44:23 -0300

    Add files via upload

---
+++ Exercícios de vetores 02/exc 6.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Digite a quantidade de pessoas: ");
+int qtd = int.Parse(Console.ReadLine());
+
+string[] nome = new string[qtd];
+int[] idade = new int[qtd];
+int maior_idade = 0;
+string mais_velho = "";
+
+for (int i = 0; i < qtd; i++)
+{
+    Console.Write($"Digite o nome da {i + 1}ª pessoa: ");
+    nome[i] = Console.ReadLine();
+
+    Console.Write($"Digite a idade de {nome[i]}: ");
+    idade[i] = int.Parse(Console.ReadLine());
+
+    if (idade[i] > maior_idade)
+    {
+        maior_idade = idade[i];
+        mais_velho = nome[i];
+    }
+}
+
+Console.WriteLine($"A pessoa mais velha é: {mais_velho}");
