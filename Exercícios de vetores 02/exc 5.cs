ae9ceafde763a52cf02d426ea945f434e553eecd
GabrielOliveiraBorges <gabrieloborges01@gmail.com>
2025-03-03 20:50:40 -0300

    Add files via upload

---
+++ Exercícios de vetores 02/exc 5.cs
@@ -0,0 +1,24 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] numeros = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+double soma = 0;
+int count = 0;
+
+foreach (int num in numeros)
+{
+ if (num % 2 == 0)
+{
+soma += num;
+count++;
+}
+}
+
+if (count > 0)
+{
+Console.WriteLine(soma / count);
+}
+else
+{
+Console.WriteLine("Nenhum número par.");
+}
+    
